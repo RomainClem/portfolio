2b5d2258fd34f2a9c002837832939b1f262a1454
Romain Clemencon <romain.clemen@gmail.com>
2026-01-28 17:28:41 +0100

    Added a game of life background

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { Hero } from "./components/sections";
+import { GameOfLife } from "./components/ui/GameOfLife";
 
 function App() {
   return (
-    <main className="min-h-screen bg-background text-foreground">
+    <main className="min-h-screen bg-background text-foreground relative">
+      <GameOfLife />
       <Hero />
     </main>
   );
